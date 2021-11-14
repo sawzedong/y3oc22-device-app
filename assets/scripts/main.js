f0e4c128f5211300d804153819796a9782979bd8
sawzedong <saw.zedong@dhs.sg>
2021-11-14 19:01:36 +0800

    Starting page added

---
+++ assets/scripts/main.js
@@ -0,0 +1,18 @@
+$(window).on( "load", function() {
+    $('#titlewrapper').addClass('magictime puffIn');
+    setTimeout(function(){
+        $('#titlewrapper').addClass('magictime puffOut');
+    }, 2500);
+    setTimeout(function() {
+        $('#titlewrapper').addClass('d-none')
+        $('#scenewrapper').removeClass('d-none')
+    }, 3550)
+})
+
+$('#v1s2link').on("click", function() {
+    window.location.href = "./v1s2.html";
+})
+
+$('#v8s1link').on("click", function() {
+    window.location.href = "./v8s1.html";
+})
